c7ee7b3037ef6c82be75e70eb649d8200d291fd4
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-07-20 10:54:07 +0300

    minor

--- samples/theta13/v4.0-neutrino2020/place.sh
+++ samples/theta13/v4.0-neutrino2020/place.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cp theta13.pdf plots/theta13_v4-0.pdf
-convert plots/theta13_v4-0.pdf plots/theta13_v4-0.png
+convert -density 300 plots/theta13_v4-0.pdf plots/theta13_v4-0.png
