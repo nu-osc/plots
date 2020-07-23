6e0f91e42cd405d9effa9e49f09972aa3b6fbf90
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-07-23 13:34:03 +0300

    Update makefile

---
+++ samples/deltaCP/v4.0-neutrino2020/pylib/converters.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+unity = lambda val, **kwargs: val
+
+def amplitude_single_double(single2, **kwrgs):
+    return 4.0*single2*(1.0-single2)
+
+def splitting_large_ee(dmee, context):
+    ordering = context['ordering']=='NO' and 1.0 or -1.0
+    dmSq21 = context['dmSq21']*1.e3 # dm32 is multiplied by 1.e3
+    sinSqTheta12 = context['sinSqTheta12']
+    cosSqTheta12 = 1.0 - sinSqTheta12
+    dm32 = dmee - ordering * cosSqTheta12 * dmSq21
+    return dm32
+
+converters = dict(
+        amplitude13=dict(single=amplitude_single_double, double=unity),
+        amplitude23=dict(single=amplitude_single_double, double=unity),
+        splitting_large={'32': unity, 'ee': splitting_large_ee}
+        )
+
+def convert(var, mode, *vals, **kwargs):
+    try:
+        fcn = converters[var][mode]
+    except KeyError:
+        raise Exception(f'Do not know how to convert {var} {mode}')
+
+    def converter(val):
+        return fcn(val, **kwargs)
+
+    return map(converter, vals)
+
