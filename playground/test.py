e456014b6d4163c87ad8401e0b100c11528aecd4
Liudmila Kolupaeva <liudmila@Mac-mini-Liudmila.local>
2020-07-24 14:02:56 +0300

    minor

--- playground/test.py
+++ playground/test.py
@@ -18,6 +18,7 @@ prop_cycle = plt.rcParams['axes.prop_cycle']
 colors = prop_cycle.by_key()['color']
 plt.rcParams['grid.alpha'] = 0.2
 plt.rcParams['grid.linewidth'] = 2
+plt.rcParams.update({'font.size': 12})
 ax = fig.add_subplot(111, projection='polar')
 colors = ['xkcd:warm purple', 'xkcd:azure', 'xkcd:green']
 offset = [1.03, 1.0, 0.96]
@@ -33,9 +34,9 @@ for exp in rev_arr:
     angle_ticks = [0, '$\pi/4$', '$\pi/2$', '3$\pi/4$', '$\pi$', '$5\pi/4$', '$3\pi/2$', '$7\pi/4$', '$2\pi$']
     ax.set_xticklabels(angle_ticks)
     #ax.map(plt.plot, "theta", "r")
-    ax.plot(float(cv), 0.2*(0.4+step*count), 'o', markersize=8, markerfacecolor=colors[count], markeredgecolor='k')
+    ax.plot(float(cv), 0.2*(0.4+step*count), 'o', markersize=8, markerfacecolor='white', markeredgecolor=colors[count])
     ax.plot( (0, float(cv)), (0, 0.2), c=colors[count], label=name.decode('utf-8'))
     ax.text(float(cv)*offset[count], 0.25, str(round(cv/pi, 2))+"$\pi$", color=colors[count])
     count = count + 1
-ax.legend(bbox_to_anchor=(1.3, 1.15), bbox_transform=ax.transAxes)
+ax.legend(bbox_to_anchor=(1.35, 1.17), bbox_transform=ax.transAxes)
 plt.show()
