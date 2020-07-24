32a274e5dfb156f6d6b7891cf6fe45ceabe6f80f
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-07-24 15:31:02 +0300

    minor

--- playground/test.py
+++ playground/test.py
@@ -1,45 +1,90 @@
 #!/usr/bin/env python
+
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 import numpy as np
-from math import cos,sin,pi
 import pandas as pd
 from matplotlib.patches import Arc
+import itertools as it
 
-
-dtype1 = np.dtype([('exp', '|S20'), ('cv', 'f8'), ('left', 'f8'), ('right', 'f8'),])
-result = np.loadtxt("../samples/deltaCP/v4.0-neutrino2020/deltaCP_NO.dat",dtype=dtype1, skiprows=1, usecols=(1, 5, 6, 7))
-print(result)
-
-fig = plt.figure()
-prop_cycle = plt.rcParams['axes.prop_cycle']
-colors = prop_cycle.by_key()['color']
+#
+# RC params
+#
+plt.rc('text', usetex=True)
 plt.rcParams['grid.alpha'] = 0.1
 plt.rcParams['grid.linewidth'] = 2
-plt.rc('text', usetex=True)
 plt.rcParams.update({'font.size': 14})
-plt.subplots_adjust(left=-0.1, right=0.9, top=0.9, bottom=0.1)
-ax = fig.add_subplot(111, projection='polar')
+
+prop_cycle = plt.rcParams['axes.prop_cycle']
+colors = prop_cycle.by_key()['color']
 colors = ['xkcd:warm purple', 'xkcd:azure', 'xkcd:green']
-offset = [1.03, 1.0, 0.96]
-count=0
-step = 0.2
+
+#
+# Load
+#
+dtype1 = np.dtype([('exp', '|S20'), ('cv', 'f8'), ('left', 'f8'), ('right', 'f8'),])
+result = np.loadtxt("../samples/deltaCP/v4.0-neutrino2020/deltaCP_NO.dat",dtype=dtype1, skiprows=1, usecols=(1, 5, 6, 7))
 rev_arr = result[::-1]
-for exp in rev_arr:
+print(result)
+
+#
+# Figure
+fig = plt.figure()
+plt.subplots_adjust(left=-0.12, right=0.85, top=0.85, bottom=0.1)
+ax = fig.add_subplot(111, projection='polar')
+ax.set_ylim(0,0.2)
+ax.set_yticks([])
+angle_ticks = [0, '$\pi/4$', '$\pi/2$', '3$\pi/4$', '$\pi$', '$5\pi/4$', '$3\pi/2$', '$7\pi/4$', '$2\pi$']
+ax.set_xticklabels(angle_ticks)
+ax.set_title('Normal Ordering', pad=15)
+
+#
+# Some stuff
+#
+offset = [1.03, 1.0, 0.96]
+step = 0.2
+center = (0.5, 0.5)
+arcopts = dict(linewidth=18, alpha=0.9, transform=ax.transAxes, lw=15)
+markeropts = dict(markersize=8, markerfacecolor='white')
+lineopts = dict(alpha=0.5)
+
+#
+# Iterate data
+#
+for count, (exp, color) in enumerate(zip(rev_arr, it.cycle(colors))):
+    # Numbers
     name, cv, left, right = exp
-    ax.set_ylim(0,0.2)
-    arc = Arc((0.5, 0.5), 0.4+step*count, 0.4+step*count, theta1=(float(cv)-float(left))/pi*180, theta2=(float(cv)+float(right))/pi*180, transform=ax.transAxes, lw=4, color=colors[count])
+    theta1=np.degrees(cv-left)
+    theta2=np.degrees(cv+right)
+    r = 0.4+step*count
+
+    # Arc
+    arc = Arc(center, r, r,
+              theta1=theta1, theta2=theta2,
+              color=color,
+              **arcopts
+              )
     ax.add_patch(arc)
-    ax.set_yticks([])
-    angle_ticks = [0, '$\pi/4$', '$\pi/2$', '3$\pi/4$', '$\pi$', '$5\pi/4$', '$3\pi/2$', '$7\pi/4$', '$2\pi$']
-    ax.set_xticklabels(angle_ticks)
-    ax.plot(float(cv), 0.2*(0.4+step*count), 'o', markersize=8, markerfacecolor='white', markeredgecolor=colors[count])
-    ax.plot( (0, float(cv)), (0, 0.2), c=colors[count], label=name.decode('utf-8'))
-    ax.text(float(cv)*offset[count], 0.25, str(round(cv/pi, 2))+"$\pi$", color=colors[count])
-    count = count + 1
-ax.legend(bbox_to_anchor=(1.0, 1.0))
+
+    # Marker
+    ax.plot(cv, step*r, 'o', markeredgecolor=color, **markeropts)
+    # Line
+    ax.plot((0, cv), (0, step), color=color, label=name.decode('utf-8'), **lineopts)
+
+    # Text
+    ax.text(cv*offset[count], 0.25, str(round(cv/np.pi, 2))+"$\pi$", color=color)
+
+#
+# Finalize the plot
+#
+ax.legend(bbox_to_anchor=(1.0, 1.0), bbox_transform=fig.transFigure)
+
 tick = [ax.get_rmax(),ax.get_rmax()*0.98]
 for t  in np.deg2rad(np.arange(0,360,15)):
     ax.plot([t,t], tick, lw=0.72, color="k")
+
+#
+# Save
+#
 plt.savefig('plot.png', dpi=300)
 plt.show()
