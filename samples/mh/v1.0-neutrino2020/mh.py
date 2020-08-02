e0b6f98dfd3623c8932d8d1768408b11a73c80ca
Liudmila Kolupaeva <liudmila@hotel205.jinr.ru>
2020-08-02 19:59:06 +0300

    make loading from yamls

--- samples/mh/v1.0-neutrino2020/mh.py
+++ samples/mh/v1.0-neutrino2020/mh.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 from matplotlib.patches import Arc, Rectangle
 import itertools as it
+import yaml
 
 def main(args):
 
@@ -24,17 +25,22 @@ def main(args):
 
     prop_cycle = plt.rcParams['axes.prop_cycle']
     colors = prop_cycle.by_key()['color']
-    colors = {'nova' : 'xkcd:warm purple', 'superkamiokande' : 'xkcd:azure', 't2k' : 'xkcd:green'}
+    colors = {'minos_2020-07-neutrino2020' : 'green', 'nova_2020-07-neutrino2020' : 'green', 't2k_2020-07-neutrino2020' : 'green', 'superk_2020-07-neutrino2020' : 'xkcd:azure', 'theor_forero_2020-06-pre-neutrino2020' : 'grey', 'theor_nufit_2020-07-post-neutrino2020' : 'grey'}
 
 #
 # Load
 #
-    filename = ''
-    if args.input:
-        filename = args.input
-    dtype1 = np.dtype([('id', '|U20'), ('exp', '|U20'), ('hie', 'f8'), ('proj', 'f8')])
-    result = np.loadtxt(filename, dtype=dtype1, usecols=(0, 1, 2, 3))
-    print(result)
+
+    filenames = ['minos_2020-07-neutrino2020', 'nova_2020-07-neutrino2020', 'superk_2020-07-neutrino2020', 't2k_2020-07-neutrino2020', 'theor_forero_2020-06-pre-neutrino2020', 'theor_nufit_2020-07-post-neutrino2020']
+    exps = []
+    for exp in filenames:
+        with open('data/'+exp+'.yaml', 'r') as f:
+            file = yaml.load(f)
+        name = file["experiment"]
+        hie = file["result"]["hier"]["value"]
+        proj = file["result"]["hier"]["proj"]
+        this_exp = {'id': exp, 'name' : name, 'hie' : hie, 'proj' : proj}
+        exps.append(this_exp)
 
 #
 # Figure
@@ -44,11 +50,10 @@ def main(args):
     ax.set_xlabel('Sigma')
     plt.subplots_adjust(left=0.25, right=0.9, top=0.9, bottom=0.15)
     ax.set_xlim(0,7.0)
-    ax.set_ylim(0.5,3.5)
-    plt.plot([5.0, 5.0], [0, 5.0], ls='--', color='grey', alpha=0.5)
+    ax.set_ylim(0.5,6.5)
+    plt.plot([5.0, 5.0], [0, 8.0], ls='--', color='grey', alpha=0.5)
     
     text_itself = []
-    text_color = []
     hie_values = []
     hie_proj = []
     order_id = []
@@ -58,37 +63,41 @@ def main(args):
     # Iterate data
     #
     styles, labels = [], []
-    for count, exp in enumerate(result):
-        id, name, hie, proj = exp
-        text_itself.append(name)
-        text_color.append(colors[id])
-        hie_values.append(hie)
-        hie_proj.append(proj)
-        order_id.append(id)
-        axis_text_1.append('\\textbf{'+str(hie)+'}')
-        axis_text_2.append(str(proj))
-        eb=plt.errorbar(proj, count+1, yerr=0.4, ls='--', color=colors[id])
+    for count, exp in enumerate(exps):
+        text_itself.append(exp['name'])
+        hie_values.append(exp['hie'])
+        hie_proj.append(exp['proj'])
+        order_id.append(exp['id'])
+        axis_text_1.append('\\textbf{'+str(exp['hie'])+'}')
+        if exp['proj'] >0:
+            axis_text_2.append(str(exp['proj']))
+        else:
+            axis_text_2.append(' ')
+        eb=plt.errorbar(exp['proj'], count+1, yerr=0.4, ls='--', color = colors[exp['id']])
         eb[-1][0].set_linestyle('--')
         
     print(hie_values)
-    y_axis = np.arange(1, 4, 1)
-    #plt.barh(y_axis, hie_values)
+    y_axis = np.arange(1, 7, 1)
+    plt.barh(y_axis, hie_values)
     barlist=plt.barh(y_axis, hie_values)
     barlist[0].set_color(colors[order_id[0]])
     barlist[1].set_color(colors[order_id[1]])
     barlist[2].set_color(colors[order_id[2]])
+    barlist[3].set_color(colors[order_id[3]])
+    barlist[4].set_color(colors[order_id[4]])
+    barlist[5].set_color(colors[order_id[5]])
     plt.yticks(y_axis, text_itself)
-    ax.set_yticks([1.5, 2.5, 3.5], minor=True)
+    ax.set_yticks([1.5, 2.5, 3.5, 4.5, 5.5, 6.5], minor=True)
     
     double_y = ax.twinx()
-    double_y.set_ylim(0.5,3.5)
+    double_y.set_ylim(0.5,6.5)
     double_y.tick_params(axis='y', direction='in', labelleft=False, labelright=True, pad=-10)
     double_y.set_yticks(y_axis)
     double_y.set_yticklabels(axis_text_1, ha='right')
     
     triple_y =  ax.twinx()
     triple_y.tick_params(axis='y', direction='out')
-    triple_y.set_ylim(0.5,3.5)
+    triple_y.set_ylim(0.5,6.5)
     triple_y.tick_params(axis='y', direction='out', labelleft=False, labelright=True, pad=10,  labelcolor='grey')
     triple_y.set_yticks(y_axis)
     triple_y.set_yticklabels(axis_text_2)
