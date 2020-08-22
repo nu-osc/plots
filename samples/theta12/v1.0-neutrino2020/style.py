624dd927efbc3aabceedf1586ce46d9bae875209
Liudmila Kolupaeva <liudmila@ratmino205.jinr.ru>
2020-08-22 19:17:05 +0300

    a few changes

--- samples/theta12/v1.0-neutrino2020/style.py
+++ samples/theta12/v1.0-neutrino2020/style.py
@@ -1,13 +1,13 @@
 colors = {
-	'kamland':         'xkcd:red',
-	'kamlandsksno':    'xkcd:azure',
-	'nova':            'xkcd:green',
-	't2k':             'xkcd:green',
-	'minos':           'xkcd:green',
-	'superkamiokande': 'xkcd:azure',
-	'icecube':         'xkcd:azure',
-	'nufit5.0':        'xkcd:steel grey',
-	'foreroetal.':     'xkcd:steel grey'
+    'kamland':         'xkcd:red',
+    'kamlandsksno':    'xkcd:orange',
+    'nova':            'xkcd:green',
+    't2k':             'xkcd:green',
+    'minos':           'xkcd:green',
+    'superkamiokande': 'xkcd:saffron',
+    'icecube':         'xkcd:azure',
+    'nufit5.0':        'xkcd:steel grey',
+    'foreroetal.':     'xkcd:steel grey'
 	}
 
 names = {
