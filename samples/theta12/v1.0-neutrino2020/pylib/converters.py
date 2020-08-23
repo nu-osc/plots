b49bb26eddf4d2765a6b999511fb46f86d7c29e3
Liudmila Kolupaeva <liudmila@ratmino205.jinr.ru>
2020-08-23 08:15:27 +0300

    added converter for theta12 tan->sin

--- samples/theta12/v1.0-neutrino2020/pylib/converters.py
+++ samples/theta12/v1.0-neutrino2020/pylib/converters.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
+import math
+
 unity = lambda val: val
 
 def amplitude_single_double(single2):
     return 4.0*single2*(1.0-single2)
+    
+def tan_to_sin(tan2):
+    return 1 - 1/(1+tan2)
 
 converters = dict(
-        amplitude12=dict(single=unity),
+        amplitude12=dict(single=unity, tan=tan_to_sin),
         amplitude13=dict(single=amplitude_single_double, double=unity),
         amplitude23=dict(single=amplitude_single_double, double=unity)
         )
