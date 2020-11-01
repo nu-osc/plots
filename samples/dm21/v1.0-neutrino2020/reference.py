08e869a4b1f3d701b77065f88fb873c612958971
Liudmila Kolupaeva <liudmila@Mac-mini-Liudmila.local>
2020-11-01 13:43:01 +0300

    fix x range

--- samples/dm21/v1.0-neutrino2020/reference.py
+++ samples/dm21/v1.0-neutrino2020/reference.py
@@ -2,7 +2,7 @@ version='1.0'
 url='git.jinr.ru/nu/osc'
 date='2020.08'
 variable=r'$\Delta m^2_{21}$, $10^{-5}$ eV$^2$'
-lims = (5.0, 8.0)
+lims = (4.0, 8.0)
 
 reference = f'v{version} {date}: {url}'
 
