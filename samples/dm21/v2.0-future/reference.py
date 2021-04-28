14ab7e92c362035fc9bf31aec64880faa4312ed8
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2021-04-28 17:15:44 +0300

    Add Δm²₂₁ with JUNO projection
    Former-commit-id: e82eff618521963a97b35c151bcdb0000fb87f90

---
+++ samples/dm21/v2.0-future/reference.py
@@ -0,0 +1,8 @@
+version='2.0b'
+url='git.jinr.ru/nu/osc'
+date='2020.08'
+variable=r'$\Delta m^2_{21}$, $10^{-5}$ eV$^2$'
+lims = (4.0, 9.5)
+
+reference = f'v{version} {date}: {url}'
+
---
+++ samples/dm21/v2.0-future/style.py
@@ -0,0 +1,18 @@
+colors = {
+    'junoreactor':     'xkcd:red',
+    'junosolar':       'xkcd:saffron',
+    'kamland':         'xkcd:red',
+    'kamlandsksno':    'xkcd:orange',
+    'nova':            'xkcd:green',
+    't2k':             'xkcd:green',
+    'minos':           'xkcd:green',
+    'superksno':       'xkcd:saffron',
+    'sno':             'xkcd:saffron',
+    'icecube':         'xkcd:azure',
+    'nufit5.0':        'xkcd:steel grey',
+    'foreroetal.':     'xkcd:steel grey'
+	}
+
+names = {
+	'Super-Kamiokande': 'SuperK+SNO'
+	}
