01f8d3b5fcc87fd1a4c45355f9ccf3dc0d572a27
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2021-07-05 15:21:54 +0300

    minor

--- samples/theta23/theta23-v2.0-future/style.py
+++ samples/theta23/theta23-v2.0-future/style.py
@@ -16,10 +16,11 @@ colors = {
 names = {
         'Super-Kamiokande': 'SuperK',
         # 'JUNO atmospheric': r'\textbf{JUNO} {\relscale{0.8}\FilledRainCloud} \relsize{-2}(expected)'
-        'JUNO atmospheric': r'\textbf{JUNO} {\relscale{0.9}\raisebox{1.5mm}{\FilledCloud}{\relscale{0.5}\hspace{-5mm}\raisebox{-0.7mm}{\Lightning\hspace{-0.8mm}\Lightning}}} \relsize{-2}(expected)'
+        # 'JUNO atmospheric': r'\textbf{JUNO} {\relscale{0.9}\raisebox{1.5mm}{\FilledCloud}{\relscale{0.5}\hspace{-5mm}\raisebox{-0.7mm}{\Lightning\hspace{-0.8mm}\Lightning}}} \relsize{-2}(expected)', # ifsym
+        'JUNO atmospheric': r'\textbf{JUNO} {\relscale{0.9}\raisebox{1.5mm}{\FilledCloud}{\relscale{0.8}\hspace{-4.7mm}\raisebox{-0.9mm}{\Lightning\hspace{-0.0mm}\Lightning}}} \relsize{-2}(expected)'
         }
 
-preamble = r'\usepackage[weather]{ifsym}\usepackage{relsize}'
+preamble = r'\usepackage[weather]{ifsym}\let\Lightning\undefined\let\Sun\undefined\usepackage{marvosym}\usepackage{relsize}'
 
 titles = dict(
     NO='Normal mass ordering',
