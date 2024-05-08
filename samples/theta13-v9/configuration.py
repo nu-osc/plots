d8d36d36de272b22aec303865b035dc63c230d7f
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2024-05-08 14:53:46 +0300

    data: remove T2K+SK θ₁₃

---
+++ samples/theta13-v9/configuration.py
@@ -0,0 +1,57 @@
+version='9'
+url='git.jinr.ru/nu/osc'
+date='2024.02'
+variables= {
+		"single": r'$\sin^2 \theta_{13}$',
+		"double": r'$\sin^2 2\theta_{13}$'
+}
+lims = None
+scale = 100.0
+
+reference = f'v{version} {date}: {url}'
+
+colors = {
+		'reactor':   'xkcd:red',
+		'reacsolar': 'xkcd:orange',
+		'solar':     'xkcd:saffron',
+		'atm':       'xkcd:azure',
+		'acc':       'xkcd:green',
+		'global':    'xkcd:steel gray',
+		}
+
+colors = {
+    'kamland':         colors['reactor'],
+    'reno':            colors['reactor'],
+    'junoreactor':     colors['reactor'],
+    'dayabay':         colors['reactor'],
+    'doublechooz':     colors['reactor'],
+    'superchooz':      colors['reactor'],
+    'dchooz':          colors['reactor'],
+    'schooz':          colors['reactor'],
+    'kamlandsksno':    colors['reacsolar'],
+    'nova':            colors['acc'],
+    't2k':             colors['acc'],
+    'minos':           colors['acc'],
+    'superkamiokande': colors['solar'],
+    'superksno':       colors['solar'],
+    'sno':             colors['solar'],
+    'icecube':         colors['atm'],
+    'desalasetal.':    colors['global'],
+    'nufit5.2':        colors['global'],
+    'pdg2023':         colors['global'],
+    'dune':            colors['acc'],
+    'capozzietal.':    colors['global']
+	}
+
+names = {
+	'JUNO reactor': r'JUNO',
+	'Super-Kamiokande': r'SuperK',
+		}
+
+titles = {}
+
+def dayabay():
+	colors['dayabay_nh'] = 'xkcd:blue'
+	colors['dayabay_ngd'] = 'xkcd:blue'
+
+preamble = r'\usepackage{xcolor}\usepackage{bbding}\let\Cross\undefined\usepackage{marvosym}\usepackage{relsize}'
